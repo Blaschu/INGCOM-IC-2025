a1a536d215874d603d67b4bee2bd868350026471
Blas <blas.pacha@gmail.com>
2025-05-07 19:15:45 -0300

    creacion de main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+print("hello worldcik")# numeral para comentar
+
+fruits = ["manzana", "Tomate", "sandia", "banannas"] # lista de frutas
+numbers = (1, 2, 3, 4, 5) # tupla de numeros #vectores
+alphabet = {"a", "b", "c", "d", "e"} # conjunto de letras
+vowels= {'a','e','i','o','u'} # conjunto de vocales #set
+
+for fruit in fruits: # bucle for para recorrer la lista de frutas
+    print(fruit) # imprime cada fruta
